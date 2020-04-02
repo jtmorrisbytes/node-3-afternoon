f6cf8b27b9095ca066c9177b50052bf732085131
= <=>
2020-04-02 17:11:30 -0500

    initial scaffolding of project

---
+++ server/index.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+
+const app = express();
+
+// since we are performing operations with the database,
+// I dont want the server to run until the database is connected
+
+massive(process.env.DATABASE_URL).then(db => {
+  app.set("db", db);
+  console.log("database connected, starting server");
+  app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+    console.log(
+      `Server listening on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+    );
+  });
+});

8c1a8b31f58b0db1080f115e3effa9b70cac8e35
= <=>
2020-04-02 18:18:01 -0500

    seeded database

--- server/index.js
+++ server/index.js
@@ -7,12 +7,16 @@ const app = express();
 // since we are performing operations with the database,
 // I dont want the server to run until the database is connected
 
-massive(process.env.DATABASE_URL).then(db => {
-  app.set("db", db);
-  console.log("database connected, starting server");
-  app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
-    console.log(
-      `Server listening on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
-    );
+massive(process.env.DATABASE_URL)
+  .then(db => {
+    app.set("db", db);
+    console.log("database connected, starting server");
+    app.listen(process.env.SERVER_PORT, process.env.SERVER_HOST, () => {
+      console.log(
+        `Server listening on http://${process.env.SERVER_HOST}:${process.env.SERVER_PORT}`
+      );
+    });
+  })
+  .catch(err => {
+    console.log("database failed to connect:", err);
   });
-});
---
+++ server/initDatabase.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const massive = require("massive");
+massive(process.env.DATABASE_URL).then(db => {
+  db.products
+    .seed()
+    .then(result => {
+      console.log("seeded products table");
+    })
+    .catch(err => {
+      console.error("failed to seed the products table", err);
+    });
+});
